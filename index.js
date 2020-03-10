f21aa2dab106216bab7abf32623a8936a04e114b
Michael Ross <mross.j@gmail.com>
2020-03-10 07:37:11 -0400

    create POST to /api/users
    * added test GET
    * added POST to check bad req and create new user
    * added 500 error saving to database

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ server.use(express.json());
 
 let users = [];
 
+// test get
+server.get('/', (req, res) => {
+	res.status(451).json('testing... unavailable for reasons?');
+});
+
+// post req, validate user --> create new user --> send errors
+server.post('/api/users', (req, res) => {
+	const userData = req.body;
+	userData.id = shortid.generate();
+	if (userData.name && userData.bio) {
+		res
+			.status(400)
+			.json({ Error: 'Please provide name and bio for the user.' });
+	} else if (users.push(userData)) {
+		res.status(201).json({ Message: 'User Created!' });
+	} else {
+		res.status(500).json({
+			Error: 'There was an error while saving the user to the database'
+		});
+	}
+});
+
 const PORT = 5000;
 server.listen(PORT, () =>
 	console.log(`\n ** API server is running on http://localhost:${PORT} ** \n`)
